b129094bd50a6413930f1835df1cfbbd19e55b8f
Evan hameed <evanhameed9966@gmail.com>
2020-05-19 14:00:30 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
+
   // your code here
+  let taskDescription= document.getElementById('new-task-description');
+  const submit= document.getElementsByTagName('input')[1];
+  let tasksList=document.getElementById('tasks');
+  const form= document.getElementById('create-task-form');
+  let tasks=[];
+
+  submit.addEventListener('click',function(event){
+
+    event.preventDefault();
+    if(taskDescription&& taskDescription.value){
+    let task ={
+
+      title: taskDescription
+    }
+    tasks.push(task);
+
+    let listItem=document.createElement('li');
+    let delBtn=document.createElement('button');
+
+    listItem.innerText= task.title.value;
+    delBtn.innerText= " x";
+
+    listItem.appendChild(delBtn);
+    tasksList.appendChild(listItem);
+
+    delBtn.onclick= function(){
+    tasksList.removeChild(listItem);
+    };
+
+    form.reset();
+
+  }
+  else{
+
+    alert("please type your task before submitting");
+  }
+  })
 });
