04914fa05894453865a060272d34f5f33e57e4e1
Zainab Saud <zai99nb@gmail.com>
2020-05-23 09:13:24 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  document.getElementById("submit").addEventListener("click",function() {
+    event.preventDefault();
+  })
+
+})
+
+function addToDo() {
+  const list= document.createElement("li");
+  const input = document.getElementById("new-task-description").value;
+  list.innerHTML = `${input}`;
+  const uList = document.getElementById("tasks");
+  uList.appendChild(list);
+  
+}
