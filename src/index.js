9fd9d1baad9eb2c378d96e2a39545f4e8db328f1
Ahmed Al-kheerow <a.alkheerow@gmail.com>
2020-05-24 00:07:13 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded",  () => {
   // your code here
+  document.getElementById('create-task-form').addEventListener('submit', function(event){
+    event.preventDefault();
+
+  })
+
+
 });
