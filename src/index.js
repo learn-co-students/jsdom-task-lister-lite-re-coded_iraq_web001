866971e021c4e49d43b91aa957c425c79439f68f
jalal Arif <jalalharfjalal@gmail.com>
2020-06-01 12:38:52 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const tasks=document.getElementById('tasks');
+  const form=document.getElementById('create-task-form');
+  const title=document.getElementById('new-task-description');
+  form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    tasks.insertAdjacentHTML(
+    "afterbegin",`<li>${title.value}</li>`
+  )
+  title.value='';
+  })
 });
