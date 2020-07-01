8b27dfe8bba758871cf05a2af8f05a0df0c3c814
Sara Nooraldin <sarasaadiq98@gmail.com>
2020-07-01 12:15:58 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   const Ltask= new Ltask()
   // your code here
+  const create-task-form = document.getElementById('create-task-form')
+  let new_task = document.getElementById("new-task-description")
+  let task = document.getElementById('tasks')
+
+  const run = () => (task.innerHtml = Ltask.renderTasks());
+
+  new_task.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    Ltask.createNewTask(createNewTask.value)
+  e.target.reset()
+  run()
+  })
+  task.addEventListener("click",(e)=>{
+    if (e.target.nodeName==="BUTTON"){
+      Ltask.deleteTask(e.target.dataset.description)
+      run()
+    }
+  })
+
+
+});
