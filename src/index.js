3668b78b4f0959811efe2fce973c4a987026ca83
Abdulbasit Salah <abdullbasit99salah@gmail.com>
2020-07-11 06:27:57 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  //grab all the necessary DOM elements
+    const tasks=document.getElementById('tasks');
+  const form=document.getElementById('create-task-form');
+  const title=document.getElementById('new-task-description');
+  form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    tasks.insertAdjacentHTML(
+    "afterbegin",`<li>${title.value}</li>`
+  )
+  title.value='';
+  })
+
+  //form and relevant input fields
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTaskPriority = document.getElementById("new-task-priority");
+
+  //ul where new tasks will live on the DOM
+  const newTaskUl = document.getElementById("tasks");
+
+  //attach event listeners
+  newTaskForm.addEventListener("submit", createNewTask);
 });
+
+const createNewTask = event => {
+  event.preventDefault();
+  //stop form from trying to submit
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+
+  appendNewTask(newTask);
+  event.target.reset();
+};
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
